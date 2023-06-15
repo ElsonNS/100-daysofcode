3b88793ff7e079e2b7f97a1801f0bbe81b5c539d
Elson Nag <99711378+ElsonNS@users.noreply.github.com>
2023-06-15 23:08:16 +0530

    Add files via upload

---
+++ Contains_Duplicate.cpp
@@ -0,0 +1,12 @@
+  class Solution {
+  public:
+      bool containsDuplicate(vector<int>& nums) {
+          unordered_map<int,int> mp;
+          for(auto i : nums) mp[i]++;
+          bool flag = false;
+          for(auto i : mp){
+              if(i.second >= 2) return true;
+          }
+          return flag;
+      }
+  };
