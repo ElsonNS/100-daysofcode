69353db331b656d4db1b1d48a381d7357116faef
Elson Nag <99711378+ElsonNS@users.noreply.github.com>
2023-06-25 00:25:23 +0530

    Add files via upload

---
+++ Remove_Element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index = 0;
+        int n = nums.size();
+        for (auto i =0; i< n; i++)
+        {
+            if(nums[i] != val)
+            {
+               nums[index] = nums[i];
+               index++;
+            }
+        }
+        return index;
+    }
+};
