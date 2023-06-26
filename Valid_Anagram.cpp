57736c94a9007ac6117b2acb841cbce7db49762e
Elson Nag <99711378+ElsonNS@users.noreply.github.com>
2023-06-26 21:25:16 +0530

    Add files via upload

---
+++ Valid_Anagram.cpp
@@ -0,0 +1,46 @@
+//Approach 1 using sorting (brute force approach)
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+        {
+           return false;
+        }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if(s == t)
+        {
+            return true;
+        }
+        return false;
+      
+        
+    }
+};
+
+//Approach 2 Using hashing 
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())
+        {
+           return false;
+        }
+        int arr[26] = {0};
+        for (int i = 0;i<s.length(); i++)
+        {
+            arr[s[i] -'a']++;
+            arr[t[i] -'a']--;
+        }
+        for (int i=0; i<26; i++)
+        {
+            if(arr[i] !=0)
+            {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
