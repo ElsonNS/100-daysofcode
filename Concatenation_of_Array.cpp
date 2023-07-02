9f96baf32c12f4e78743d77eb59da0c67929f0fc
Elson Nag <99711378+ElsonNS@users.noreply.github.com>
2023-07-02 20:07:05 +0530

    Add files via upload

---
+++ Concatenation_of_Array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i<n; i++)
+        {
+            nums.emplace_back(nums[i]);
+        }
+        return nums;
+    }
+};
+
+// Approach 2
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans(2*nums.size());
+       for(int i = 0; i<nums.size(); i++)
+       {
+           ans[i] = nums[i];
+           ans[i+nums.size()] = nums[i];
+       }
+       return ans;
+    }
+};
+
