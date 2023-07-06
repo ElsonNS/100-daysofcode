9de49d28370994b14883c15d5070945f46a70c46
Elson Nag <99711378+ElsonNS@users.noreply.github.com>
2023-07-06 21:55:30 +0530

    Add files via upload

---
+++ Convert_the_Temperature.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        vector<double> temp;
+        temp.push_back(celsius + 273.15);
+        temp.push_back((celsius * 1.80) + 32);
+        return temp;
+    }
+};
